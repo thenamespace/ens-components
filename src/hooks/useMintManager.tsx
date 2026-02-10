12e4cee35516f1275b5c722a218436999ff364c0
arti <nenadmitt@gmail.com>
2026-02-10 15:25:31 +0100

    Fix minting process for sepolia

--- src/hooks/useMintManager.tsx
+++ src/hooks/useMintManager.tsx
@@ -1,6 +1,6 @@
 import { ListingNetwork, NameListing } from "@/types";
 import { createMintClient } from "@thenamespace/mint-manager";
-import { useCallback, useMemo } from "react";
+import { useMemo } from "react";
 
 const LIST_MANAGER_API = "https://list-manager.namespace.ninja";
 const LIST_MANAGER_TESTNET_API = "https://staging.list-manager.namespace.ninja";
@@ -11,28 +11,24 @@ interface UseMintManagerParams {
 
 export const useMintManager = ({ isTestnet }: UseMintManagerParams) => {
   const mintClient = useMemo(() => {
+     console.info(`Creating a mint clinet, testnet: ${isTestnet}`,)
     return createMintClient({
       cursomRpcUrls: {},
       isTestnet: isTestnet || false,
     });
   }, [isTestnet]);
 
-  const getListManagerApi = useCallback(() => {
-    return isTestnet ? LIST_MANAGER_TESTNET_API : LIST_MANAGER_API;
-  }, [isTestnet]);
-
-  const getListingNetwork = useCallback(() => {
-    return isTestnet ? ListingNetwork.Sepolia : ListingNetwork.Mainnet;
-  }, [isTestnet]);
-
-  const getListingDetails = useCallback(
-    async (name: string): Promise<NameListing> => {
-      return fetch(
-        `${getListManagerApi()}/api/v1/listing/network/${getListingNetwork()}/name/${name}`
-      ).then((res) => res.json());
-    },
-    [getListManagerApi, getListingNetwork, isTestnet]
-  );
+  const getListingDetails = async (name: string): Promise<NameListing> => {
+    const listManagerApi = isTestnet
+      ? LIST_MANAGER_TESTNET_API
+      : LIST_MANAGER_API;
+    const listingNetwork = isTestnet
+      ? ListingNetwork.Sepolia
+      : ListingNetwork.Mainnet;
+    return fetch(
+      `${listManagerApi}/api/v1/listing/network/${listingNetwork}/name/${name}`
+    ).then(res => res.json());
+  };
 
   return {
     mintClient,
