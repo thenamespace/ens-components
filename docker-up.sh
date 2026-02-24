83295b7d9f337f797f96a5800f23541192b91b7d
arti <nenadmitt@gmail.com>
2026-02-24 22:43:37 +0100

    Add 1.0.0 base version

--- docker-up.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Script to always rebuild and run the Storybook container
-docker-compose up --build --force-recreate
-
--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -1,22 +1,8 @@
-// Styles - Import CSS so consumers get Bootstrap and other styles
 import "./styles/index.css";
-
-// Components
 export * from "./components";
-
-// Constants
 export * from "./constants";
-
-// Types
 export * from "./types";
-
-// Utils
 export * from "./utils";
-
-// Web3
 export * from "./web3";
-
-// Theme
 export * from "./theme/ThemeProvider";
-
 export * from "./hooks";
